e07d6c955177a22423c35535e1bbb00ac3209fb7
baran61 <azak.baran99@gmail.com>
2023-11-16 17:49:48 +0000

    changes

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+ const Home = () =>{
+  return (
+    <div>
+      <h1>Performance and Suitability Analysis</h1>
+      <p>
+        In this section, we conduct an in-depth analysis of the performance and
+        suitability aspects of our application.
+      </p>
+      <h2>Performance Metrics</h2>
+      <p>
+        We measure various performance metrics such as load times, responsiveness,
+        and resource utilization to ensure optimal performance.
+      </p>
+      <h2>Suitability Evaluation</h2>
+      <p>
+        Our evaluation includes assessing the suitability of the application for
+        different use cases, scalability, and compliance with requirements.
+      </p>
+    </div>
+    
+  );
+};
+
+export default Home;
